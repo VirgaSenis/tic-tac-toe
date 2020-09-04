a78e13ad96ba6cafacb1cd1078e120b03a57d853
Yoonho Choi <yoonho_choi@hotmail.com>
2020-09-04 00:25:27 -0400

    Updates

---
+++ precache-manifest.15f51006b2c4b3f9b49bc2446bdd5689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daabcf15a839a74341d4dd4457300b2f",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "19de0df62e9e39bbfffd",
+    "url": "/tic-tac-toe/static/css/main.58ddedc1.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19de0df62e9e39bbfffd",
+    "url": "/tic-tac-toe/static/js/main.d8084c0d.chunk.js"
+  },
+  {
+    "revision": "4ad95f08d6b4d56d4069",
+    "url": "/tic-tac-toe/static/js/runtime-main.fa7aabe4.js"
+  }
+]);
